85559b3fdf1ca51c72e9ffca89b708a9a5bc329e
Chris Han <zero@seas.upenn.edu>
2023-12-19 03:54:14 -0500

    production readiness
    add prod CORS origins

--- omo_api/db/init/initialize.sql
+++ omo_api/db/init/initialize.sql
@@ -1,11 +1,11 @@
 -- This sql file is automated executed when the postgres container is first created
 -- These values are hardcoded and doesn't use env variables :(
-CREATE USER omoai WITH PASSWORD 'omoai';
+CREATE USER omoai WITH PASSWORD 'CHANGE_ME';
 CREATE DATABASE omoai;
 
--- grant necessary privileges to simpledb user
+-- grant necessary privileges to user
 GRANT ALL PRIVILEGES ON DATABASE omoai TO omoai;
 GRANT ALL ON SCHEMA public TO omoai;
 
--- ensure simpledb owner can make changes
+-- ensure owner can make changes
 ALTER DATABASE omoai OWNER TO omoai;
--- omo_api/main.py
+++ omo_api/main.py
@@ -1,17 +1,11 @@
 import os
-import pinecone
 import logging
 from logging.config import dictConfig
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langchain import hub
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.runnable import RunnablePassthrough
-from slack_sdk import WebClient
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware # TODO
 from omo_api.conf.log import log_config
-from omo_api.models.slack import SlackPayload
 from omo_api.routers import (
     auth_router,
     files_router, 
@@ -20,7 +14,6 @@ from omo_api.routers import (
     slack_router,
 )
 from omo_api.db.connection import engine
-from omo_api.db.models.common import DeclarativeBase
 
 # Necessary for the call to create_all() to create tables
 from omo_api.db.models import *
@@ -36,11 +29,21 @@ app.include_router(confluence_router.router)
 app.include_router(slack_router.router)
 
 Base.metadata.create_all(bind=engine)
+ 
+APP_ENV = os.getenv('APP_ENV', 'development')
 
-origins = [
-    "http://localhost:8000",
-    "http://localhost:5173",
-]
+if APP_ENV == 'production':
+    origins = [
+        "https://api.omo.bot",
+        "https://app.helloomo.ai",
+    ]
+else:
+
+    origins = [
+        "http://localhost:8000",
+        "http://localhost:5173",
+    ]
+
 
 app.add_middleware(
     CORSMiddleware,
