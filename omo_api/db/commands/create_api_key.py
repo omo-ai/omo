b91979fa6c125b17254357982e0cb039f0b247f0
Chris Han <zero@seas.upenn.edu>
2024-05-01 04:58:21 -0400

    bugfixes

--- omo_api/db/commands/create_api_key.py
+++ omo_api/db/commands/create_api_key.py
@@ -1,19 +1,18 @@
 import sys
-import secrets
 import click
 from sqlalchemy import select
 from sqlalchemy.exc import MultipleResultsFound
-from omo_api.db.models import UserAPIKey, User
+from omo_api.db.models import APIKey, User
 from omo_api.db.connection import session
-from omo_api.utils.auth import create_api_key
-from omo_api.settings import cipher_suite
+from omo_api.utils import create_api_key, get_api_key_hash
 
 @click.command()
 @click.option('--userid', help='User ID')
-def create_api_key(userid):
+def create_user_api_key(userid):
     """Generate an API key for a user ID"""
     max_attempts = 5
     num_attempts = 0
+
     stmt = select(User).where(User.id == userid)
     user = session.execute(stmt).one_or_none()
 
@@ -25,20 +24,27 @@ def create_api_key(userid):
     
     while num_attempts < max_attempts:
 
-        key = create_api_key()
+        raw_api_key = create_api_key()
+        hashed_key = get_api_key_hash(raw_api_key)
 
         try:
-            result = session.query(UserAPIKey).filter(UserAPIKey.api_key == key).count()
+            result = session.query(APIKey).filter(APIKey.hashed_api_key == hashed_key).count()
 
             if result > 0:
                 raise MultipleResultsFound
 
             click.echo('Creating new API Key...')
-            user_api_key = UserAPIKey(user_id=user.id, api_key=key)
+            user_api_key = APIKey(user_id=user.id, hashed_api_key=hashed_key)
 
             session.add(user_api_key)
             session.commit()
-            click.echo(f"API Key created: {key}")
+            output = f"""
+            API Key created! Save this key somewhere safe. We'll only display
+            this once below. If you lose this, you'll have to generate a
+            new API key.
+            API Key: {raw_api_key}
+            """
+            click.echo(output)
 
             sys.exit(0) # return successfully
 
@@ -54,4 +60,4 @@ def create_api_key(userid):
 
 
 if __name__ == '__main__':
-    create_api_key()
+    create_user_api_key()
--- omo_api/utils/__init__.py
+++ omo_api/utils/__init__.py
@@ -1,14 +1,22 @@
 from .base import get_env_var, clean_url, flatten_list
-from .auth import verify_google_access_token
+from .auth import (
+    verify_google_access_token,
+    create_api_key,
+    verify_api_key,
+    get_api_key_hash
+)
 from .vector_store import get_current_vector_store
 from .background import get_celery_task_status, display_task_status
 
 __all__ = [
-    'verify_google_access_token',
     'flatten_list',
     'clean_url',
     'get_env_var',
     'get_current_vector_store',
     'get_celery_task_status',
     'display_task_status',
+    'verify_google_access_token',
+    'create_api_key',
+    'verify_api_key',
+    'get_api_key_hash',
 ]
